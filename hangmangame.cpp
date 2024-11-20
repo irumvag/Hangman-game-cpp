baefa0741c15920f4ab7fa346e0c49ca77e14198
irumvag <irumvag@kalizeroC.ur.ac.rw>
2024-11-20 12:38:11 +0200

    All is working well now

--- hangmangame.cpp
+++ hangmangame.cpp
@@ -11,13 +11,12 @@ class hangman{
     public:
     vector<string> wordlist={"kigali","apple", "banana", "cherry", "date", "elderberry"};
     vector<char> found;
-    string n,word;
-    char y;
-    int results=0,counter=0,x;
+    string n,word,y;
+    int results=0,counter=0,x,g;
 
     hangman(){
     srand(time(0));
-    int g=rand()%wordlist.size();
+    g=rand()%wordlist.size();
     word=wordlist[g];
     x=word.size();
     static int i;
@@ -58,10 +57,21 @@ class hangman{
             cout<<"\nYou Guessed collect!!\n\n\n    ";
             for(auto w:found)
             {
-                cout<<w;
+                cout<<w<<" ";
             }
             cout<<"\n\nYou have "<<(15-counter)<<" Guess remain?, Good Luck!\n\n";
             results=0;
+            int k=0;
+            for(auto w:found)
+                {
+                if(w!='_')
+                    k++;
+                }
+            if(k==word.size())
+                {
+                cout<<"\nCongratation!!! You have won the game!!";
+                reset();
+                }
         }
         else if(results==0){
             if(counter<=15)
@@ -69,7 +79,7 @@ class hangman{
             cout<<"\nYou Guessed  not collect!!\n\n\n    ";
             for(auto w:found)
             {
-                cout<<w;
+                cout<<w<<" ";
             } 
             cout<<"\n\nYou have "<<(15-counter)<<" Guess remain?, Good Luck!\n\n"; 
             }
@@ -78,43 +88,53 @@ class hangman{
     void reset(){
         if(counter==15)
         cout<<"\n\nYou are out of Guess!!!!\n\n";
-        cout<<"\n\n Do you want to try again(Y/N):";
+        cout<<"\n\n Do you want to play again(Y/N):";
         cin>>y;
-        if(y=='y'||y=='Y')
-        {
-            found.clear();
-            hangman();
-            counter=0;
-            results=0;            
-        }
+        if((y.size()==1&&isalpha(y[0]))&&(!isspace(y[0]))&&(!isdigit(y[0]))&&(y[0]=='n'||y[0]=='N')||(y[0]=='y'||y[0]=='Y'))
+            {
+            if(y[0]=='y'||y[0]=='Y')
+            {
+            system("clear");
+            resetvariable();            
+            }
+            }
+        else 
+            {
+                cout<<"\nInvalid input! try again?\n\n";
+                reset();
+            }
+    }
+    void resetvariable(){
+        counter=0;
+        y='\0';
+        srand(time(0));
+        g=rand()%wordlist.size();
+        word=wordlist[g];
+        x=word.size();
+        found.clear();
+        for(int i=0;i<x;i++)
+        found.push_back('_');
+        cout<<"     You are most Welcome in hangman game!\n";
+        cout<<"     \n\nHangman Game tips and procedure: \n    There is a word that is hidden and that word has letters in 27 letters (A-Z).\n    The hangman game is all about to guess the letter in that word,\n    So it's only allowed to insert only one letter! with 15 guesses per word Goodluck!!\n\n    Game start now..............\n\n  ";
     }
 };
 int main(){
     hangman wlist;
-    for(int a=0;a==0;a){
+    for(int a=0;a==0;a)
+    {
     wlist.userinput();
     wlist.checking();
     wlist.output();
-    int p=0,k=0;
-        for(auto w:wlist.found)
-        {
-            if(w!='_')
-            k++;
-            p++;
-        }
-        if(k==wlist.word.size()){
-            cout<<"\nCongratation!!! You have won the game!!";
+    if(wlist.counter==15)
+    {
+            cout<<"\n\nYou failed the game!!!\n\n";
             wlist.reset();
-        }
-        else if(wlist.counter==15)
-        {
-            wlist.reset();
-            if(wlist.y=='n'||wlist.y=='N')
+            if(wlist.y[0]=='n'||wlist.y[0]=='N')
                 {
                     cout<<"\n\nThanks for playing the game!!";
                     break;
                 }
-        }
+    }
     }
     return 0;
 }
